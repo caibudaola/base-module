6c80cc71e28f44f6b249f881f3901a3bd628fc48
linchuan <jtnddblm@gmail.com>
2019-03-11 07:36:22 +0800

    add mail

---
+++ src/Module/Base/Mail/CustomMail.php
@@ -0,0 +1,30 @@
+<?php
+namespace Module\Base\Mail;
+
+use Exception;
+use Symfony\Component\Debug\Exception\FlattenException;
+use Mail;
+use Symfony\Component\Debug\ExceptionHandler as SymfonyExceptionHandler;
+
+class CustomMail {
+
+	public function __construct($config)
+	{
+        $this->debug = isset($config['debug']) ? $config['debug'] : 'false';
+	}
+
+    public function sendEmail(Exception $exception)
+    {
+        try {
+            $e = FlattenException::create($exception);
+
+            $handler = new SymfonyExceptionHandler();
+
+            $html = $handler->getHtml($e);
+
+            Mail::to(env('MAIL_EXCEPTION_RECEIVER', 'lzw122333@gmail.com'))->send(new ExceptionOccured($html));
+        } catch (Exception $ex) {
+            dd($ex);
+        }
+    }
+}
---
+++ src/Module/Base/Mail/Mail/ExceptionOccured.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Module\Base\Mail\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ExceptionOccured extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * The body of the message.
+     *
+     * @var string
+     */
+    public $content;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.exception')
+            ->with('content', $this->content)
+            ->subject(env('APP_NAME').'-异常告警');
+    }
+}
